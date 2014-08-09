6fb25ddae463e4fe9874e26fc927f806157cd81c
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-08-09 10:03:07 +0300

    exapmple fl.ru

---
+++ portf/example/script.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+
+		var $link = $('.link'),
+		    $block = $('.block__item');
+
+		    $link.on('click', function (e) {
+		    	e.preventDefault();
+
+		    	var that     = $(this), 
+		    		dataLink = that.data('link');
+		    	
+		    	$block.hide();
+
+		    	$block.each(function(index, el) {
+		    		if ($(this).data('link') == dataLink) {
+		    			$(this).fadeIn('fast');
+		    		}
+		    	});
+
+		    })
+
+	});
