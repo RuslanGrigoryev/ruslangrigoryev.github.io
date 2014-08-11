ae7a837400a0f3d8de268b352273469d87ab4917
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-08-11 19:29:12 +0300

    Fixed portfolio + start test Ipad

--- js/script.js
+++ js/script.js
@@ -21,34 +21,3 @@
 
 
 });
-
-function loadHTML(sURL)
- 
-{
-  var request=null;
-  // пытаемся создать объект для MSXML 2 и старше
-  if(!request) try {
-    request=new ActiveXObject('Msxml2.XMLHTTP');
-  } catch (e){}
-  // не вышло... попробуем для MSXML 1
-  if(!request) try {
-    request=new ActiveXObject('Microsoft.XMLHTTP');
-  } catch (e){}
-  // не вышло... попробуем для Mozilla
-  if(!request) try {
-    request=new XMLHttpRequest();
-  } catch (e){}
-  if(!request)
-    // ничего не получилось...
-    return "";
-  // делаем запрос
-  request.open('GET', sURL, false);
-  request.send(null);
-  // возвращаем текст
-  return request.responseText;
-}
- 
-function openPage (sURL) {
-	mypagecontent = document.getElementById('page')
-	mypagecontent.innerHTML = loadHTML(sURL);
-}
