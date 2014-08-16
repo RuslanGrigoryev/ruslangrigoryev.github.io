d0ef41d4c1272821f85dcc7eb955753b4b1d984e
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-08-16 12:59:52 +0300

    Плавное скольжение

---
+++ portf/v/js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+
+	/*click*/
+	var $btn    = $('#jsBtnLink'),
+	    $bannerHeight = $('#banner').height();
+
+	    $btn.on('click', function (e) {
+	    	e.preventDefault();
+
+	    	console.log($bannerHeight);
+
+	    	$('html, body').animate({
+	    	        scrollTop: $("#banner").offset().top + $bannerHeight
+	    	}, 1000);
+	    })
+});
