be678d2fe69083ac3be2ffb3768b54baaae1a4c3
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-08-19 20:16:05 +0300

    geo blocks

---
+++ portf/geo/js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+	$('img').each(function (i) {
+		$(this).width($(this).width()/1.3);
+		$(this).height($(this).height()/1.3);
+	});
+
+	$('.content').height($(window).height() );
+});
