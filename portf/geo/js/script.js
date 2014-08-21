0a5391583366c4660d442125576906fce0ff459a
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-08-21 13:07:20 +0300

    Убрал скрипты

--- portf/geo/js/script.js
+++ portf/geo/js/script.js
@@ -1,13 +1,8 @@
-$(function () {
-	$('.img').each(function (i) {
+/*$(function () {
+	$('img').each(function (i) {
 		$(this).width($(this).width()/1.3);
 		$(this).height($(this).height()/1.3);
 	});
 
-
-
-
-	
-
 	$('.content').height($(window).height() );
-});
+});*/
