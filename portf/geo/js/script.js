d36603d6bdff737739434b4cd043230913894f3c
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-08-21 13:49:17 +0300

    Убрал скрипты, добавил атрибуты к картинкам

--- portf/geo/js/script.js
+++ portf/geo/js/script.js
@@ -1,9 +1,4 @@
 $(function () {
-	$('img').each(function (i) {
-		$(this).width($(this).width()/1.3);
-		$(this).height($(this).height()/1.3);
-		console.log($(this).attr('src'));
-	});
 
 	$('.content').height($(window).height() );
 });
