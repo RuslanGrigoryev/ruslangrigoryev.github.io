6960f640c18e55e4e1c9c535bcab702f0e72c615
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-08-21 13:14:09 +0300

    поправил баг

--- portf/geo/js/script.js
+++ portf/geo/js/script.js
@@ -1,8 +1,9 @@
-/*$(function () {
+$(function () {
 	$('img').each(function (i) {
 		$(this).width($(this).width()/1.3);
 		$(this).height($(this).height()/1.3);
+		console.log($(this).attr('src'));
 	});
 
-});*/
 	$('.content').height($(window).height() );
+});
