cf3e39403a73d52dc64337b0458f4ed3b4fef95b
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-08-30 17:08:22 +0300

    Добавил карту Италии для просмотра заказчиком

---
+++ portf/italy/js/script.js
@@ -0,0 +1,11 @@
+$('#italy').mapster({
+      render_highlight: {
+          fillColor: 'dec098',
+          stroke: false
+      },
+      render_select: {
+          fillColor: 'd28e48',
+          stroke: false
+      },
+      fadeInterval: 50
+});
---
+++ portf/test-task/js/main.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+	/*Slider*/
+	var $link  = $('.b-slider-case-element'),
+	    $slide = $('.b-slide');
+
+	$link.on('click', function (e) {
+		e.preventDefault();
+
+		var $self = $(this),
+		    $selfData = $self.data('link');
+
+		$slide.each(function () {
+			if ( $(this).data('slide') == $selfData ) {
+				$slide.removeClass('b-slide_active');
+				$(this).addClass('b-slide_active');
+			}
+		});
+
+		$link.find('.b-slider-case-element__number').removeClass('b-slider-case-element__number_active');
+		$link.find('.b-slider-case-element__text').removeClass('b-slider-case-element__text_active');
+		$self.find('.b-slider-case-element__number').addClass('b-slider-case-element__number_active');
+		$self.find('.b-slider-case-element__text').addClass('b-slider-case-element__text_active');
+	}) 
+
+	/*Main menu*/
+
+	$('.b-link_menu').on('click', function () {
+		$('.b-link_menu').removeClass('b-link_menu_active');
+		$(this).addClass('b-link_menu_active');
+	});
+});
